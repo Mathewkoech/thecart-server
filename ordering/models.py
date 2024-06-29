9eb0a458e15f47adefaf31afb28de1cd49c16862
mathewkoech <mathewkoech55@gmail.com>
2024-06-29 23:20:06 +0300

    initial data

--- ordering/models.py
+++ ordering/models.py
@@ -37,7 +37,7 @@ class Order(TimeStampedModelMixin, FlaggedModelMixin):
     )
     status = models.CharField(max_length=30, choices=STATUS_CHOICES, default='pending')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    billing_address = models.TextField()
+    billing_address = models.CharField(max_length=255)
     shipping_address = models.ForeignKey(Shipping, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -50,7 +50,7 @@ class OrderItem(TimeStampedModelMixin):
     """
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
